5df35752e6035a4bddd6d8cd93b9d22820553249
talhanonari <asimriaz1998@gmail.com>
2024-06-07 17:45:16 +0500

    Revert "Authenticatio With JWT"
    This reverts commit 3b58301d664108b54f5ae1539da69553582b4fb8.

--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -1,61 +1,16 @@
 const user = require('../Model/userSchema');
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
-exports.UserRegister = async (req, res) => {
+exports.createUser = async (req, res) => {
     try {
-        const hashed = bcrypt.hashSync(req.body.password, 10);
-        const userData = new user({
-            name: req.body.name,
-            email: req.body.email,
-            password: hashed
-        });
-
-        await userData.save();
+        let data = new user(req.body);
+    let result = await data.save();
         res.status(200).json({
-            message: "User Register"
+            message: "User Created Successfully"
         });
     } catch (error) {
         console.log(error);
         res.status(400).json({
-            message: "User cannot be Register"
-        });
-    }
-};
-
-exports.UserLogin = async (req, res) => {
-    const { email, password } = req.body;
-    if (email && password) {
-        const userData = await user.findOne({ email: email });
-        if (userData) {
-            const comPass = await bcrypt.compare(password, userData.password);
-            if (userData.email === email && comPass) {
-                jwt.sign({ name: userData.email }, "seckey", (err, token) => {
-                    if (token) {
-                        res.status(200).json({
-                            message: "user successfully login ",
-                            token,
-                            userData
-                        });
-                    } else {
-                        res.status(400).json({
-                            message: "token cannot be created"
-                        });
-                    }
-                });
-            } else {
-                res.status(400).json({
-                    message: "please enter your correct password"
-                });
-            }
-        } else {
-            res.status(400).json({
-                message: "please enter your correct email"
-            });
-        }
-    } else {
-        res.status(400).json({
-            message: "please enter all field"
+            message: "User Cannot Created"
         });
     }
 };
@@ -72,29 +27,12 @@ exports.showUser = async (req, res) => {
     }
 };
 
-exports.showOneUser = async (req, res) => {
-    try {
-        let data = await user.findOne(req.params);
-        res.status(200).send(data);
-    } catch (error) {
-        res.status(400).json({
-            message: "No Data Found Against this user ID",
-            error
-        });
-    }
-};
-
 exports.updateUser = async (req, res) => {
     try {
-        const hashed = bcrypt.hashSync(req.body.password, 10);
         let data =await user.updateOne(
             req.params,
             {
-                $set:{
-                    name: req.body.name,
-                    email: req.body.email,
-                    password: hashed
-                }
+                $set:req.body
             }
         );
         res.status(200).json({
--- src/Middleware/tokenVerify.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-exports.jwtVerifyToken = (req, res, next) => {
-    const token = req.headers.token;
-    const decoded = jwt.verify(token, "seckey");
-    next();
-};
