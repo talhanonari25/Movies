3b58301d664108b54f5ae1539da69553582b4fb8
talhanonari <asimriaz1998@gmail.com>
2024-06-07 17:42:28 +0500

    Authenticatio With JWT

--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -1,16 +1,61 @@
 const user = require('../Model/userSchema');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-exports.createUser = async (req, res) => {
+exports.UserRegister = async (req, res) => {
     try {
-        let data = new user(req.body);
-    let result = await data.save();
+        const hashed = bcrypt.hashSync(req.body.password, 10);
+        const userData = new user({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashed
+        });
+
+        await userData.save();
         res.status(200).json({
-            message: "User Created Successfully"
+            message: "User Register"
         });
     } catch (error) {
         console.log(error);
         res.status(400).json({
-            message: "User Cannot Created"
+            message: "User cannot be Register"
+        });
+    }
+};
+
+exports.UserLogin = async (req, res) => {
+    const { email, password } = req.body;
+    if (email && password) {
+        const userData = await user.findOne({ email: email });
+        if (userData) {
+            const comPass = await bcrypt.compare(password, userData.password);
+            if (userData.email === email && comPass) {
+                jwt.sign({ name: userData.email }, "seckey", (err, token) => {
+                    if (token) {
+                        res.status(200).json({
+                            message: "user successfully login ",
+                            token,
+                            userData
+                        });
+                    } else {
+                        res.status(400).json({
+                            message: "token cannot be created"
+                        });
+                    }
+                });
+            } else {
+                res.status(400).json({
+                    message: "please enter your correct password"
+                });
+            }
+        } else {
+            res.status(400).json({
+                message: "please enter your correct email"
+            });
+        }
+    } else {
+        res.status(400).json({
+            message: "please enter all field"
         });
     }
 };
@@ -27,12 +72,29 @@ exports.showUser = async (req, res) => {
     }
 };
 
+exports.showOneUser = async (req, res) => {
+    try {
+        let data = await user.findOne(req.params);
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(400).json({
+            message: "No Data Found Against this user ID",
+            error
+        });
+    }
+};
+
 exports.updateUser = async (req, res) => {
     try {
+        const hashed = bcrypt.hashSync(req.body.password, 10);
         let data =await user.updateOne(
             req.params,
             {
-                $set:req.body
+                $set:{
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hashed
+                }
             }
         );
         res.status(200).json({
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const User = require('../Controller/userController');
 const Review = require('../Controller/reviewController')
+const token = require("../Middleware/tokenVerify");
 const Route = express.Router();
 
-Route.post("/create", User.createUser);
-Route.get("/show", User.showUser);
-Route.put("/update/:_id", User.updateUser);
-Route.delete("/delete/:_id", User.deleteUser);
+Route.post("/signup", User.UserRegister)
+Route.post("/login", User.UserLogin);
 
-Route.post("/addreview", Review.addReview);
-Route.get("/showreviews", Review.showReviews);
-Route.get("/showreviews/:_id", Review.showOneReview)
-Route.put("/updatereview/:_id", Review.updateReview);
-Route.delete("/deletereview/:_id", Review.deleteUser);
+Route.get("/user",token.jwtVerifyToken, User.showUser);
+Route.put("/user/update/:_id", token.jwtVerifyToken, User.updateUser);
+Route.delete("/user/delete/:_id", token.jwtVerifyToken, User.deleteUser);
+
+Route.post("/review/add", token.jwtVerifyToken, Review.addReview);
+Route.get("/review", token.jwtVerifyToken, Review.showReviews);
+Route.get("/review/:_id", token.jwtVerifyToken, Review.showOneReview)
+Route.put("/review/update/:_id", token.jwtVerifyToken, Review.updateReview);
+Route.delete("/review/delete/:_id", token.jwtVerifyToken, Review.deleteUser);
 
 module.exports = Route;
